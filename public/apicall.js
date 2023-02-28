dfd2772710de860d9f4d66efd4dd99feb14be873
joshua ng <joshuang_443@hotmail.com>
2023-02-28 09:52:54 +0900

    code

--- public/apicall.js
+++ public/apicall.js
@@ -13,7 +13,7 @@ let temp_user;
 
 // const url = "http://localhost:3000"
 
-const url  = "https://chirpvault-joshnsw.onrender.com"
+const url  = "https://chirpvault.onrender.com"
 
 
 getTweet.addEventListener("click", function() {
